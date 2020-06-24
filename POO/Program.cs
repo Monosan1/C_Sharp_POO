bfe54ae252d963ccd859c8dfc59515b49df39b9a
Gabriel Braga <araujogabriel2010@gmail.com>
2020-06-24 10:33:02 -0300

    Alterado o nome do Arquivo

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace POO
 {
@@ -6,7 +7,16 @@ namespace POO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double xA, xB, xC, yA, yB, yC;
+            Console.WriteLine("Entre com as medidadas do trinagulo X");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com as medidadas do triangulo Y");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
         }
     }
 }
