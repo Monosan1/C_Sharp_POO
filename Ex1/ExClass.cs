e73cd7f75af2c06ea1d5ae73eef1bc8339fa6f70
Gabriel Braga <araujogabriel2010@gmail.com>
2020-06-25 13:15:43 -0300

    Criei os exercícios EX1

---
+++ Ex1/ExClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex1
+{
+    class ExClass1
+    {
+        public string Name;
+        public int Age;
+    }
+
+    class ExClass2
+    {
+        public string Name;
+        public double Salary;
+        public double Averege;
+    }
+
+}
+
---
+++ Ex1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+
+/*namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExClass1 x, y;
+
+            x = new ExClass1();
+            y = new ExClass1();
+
+            Console.WriteLine("Digite um nome:");
+            x.Name = Console.ReadLine();
+            Console.WriteLine("Digite a idade:");
+            x.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro nome:");
+            y.Name = Console.ReadLine();
+            Console.WriteLine("Digite a idade:");
+            y.Age = int.Parse(Console.ReadLine());
+
+            if (x.Age > y.Age)
+            {
+                Console.WriteLine($"Pessoa mais velha:{x.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"Pessoa mais velha:{y.Name}");
+            }
+
+        }
+    }
+}*/
+
+namespace Ex1
+{
+    class Ex2
+    {
+        static void Main()
+        {
+            ExClass2 x, y;
+
+            x = new ExClass2();
+            y = new ExClass2();
+
+            Console.WriteLine("Digite um nome:");
+            x.Name = Console.ReadLine();
+            Console.WriteLine("Digite a Salario:");
+            x.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture) ;
+            Console.WriteLine("Digite outro nome:");
+            y.Name = Console.ReadLine();
+            Console.WriteLine("Digite a Salario:");
+            y.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            x.Averege = (x.Salary + y.Salary) / 2;
+            Console.WriteLine($"A Média é de: {x.Averege.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
