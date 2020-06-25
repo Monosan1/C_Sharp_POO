ad84687379686fce00df606f3ce79020af9971f7
Gabriel Braga <araujogabriel2010@gmail.com>
2020-06-25 14:36:36 -0300

    Adicionado o método Override .ToString()

---
+++ Estoque/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Estoque
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+            {
+            return Preco * Quantidade;
+            }
+        public override string ToString()
+            {
+            return Nome + ", R$ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " Unidades, Total: R$"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+                
+            }
+        }
+}
--- Estoque/Program.cs
+++ Estoque/Program.cs
@@ -2,12 +2,22 @@
 using System.Globalization;
 
 namespace Estoque
-{
-    class Program
     {
-        static void Main(string[] args)
+    class Program
         {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args)
+            {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados dos Produtos");
+            Console.WriteLine($"Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Qauntidade no Estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do Produto: {p}");
+            }
         }
     }
-}
