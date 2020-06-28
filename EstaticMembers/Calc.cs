f5b1d2767b1f35afcea422532da054e0e643fd66
Gabriel Braga <araujogabriel2010@gmail.com>
2020-06-28 00:43:59 -0300

    Classe Calc criada.

---
+++ EstaticMembers/Calc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstaticMembers
+    {
+    class Calc
+        {
+        public double Pi = 3.14;
+
+        public double Circunferencia(double r)
+            {
+            return 2 * Pi * r;
+            }
+        public double Volume(double r)
+            {
+            return 4 * Pi * Math.Pow(r, 3) / 3;
+            }
+        }
+    }
--- EstaticMembers/Program.cs
+++ EstaticMembers/Program.cs
@@ -6,27 +6,21 @@ namespace EstaticMembers
     class Program
         {
 
-        static double Pi = 3.14;
+        
         static void Main(string[] args)
             {
+            Calc Circ1;
+
+            Circ1 = new Calc();
+
             Console.WriteLine("Entre com o valor do Raio: ");
-            double raio = double.Parse(Console.ReadLine());
+            double raio = double.Parse(Console.ReadLine());           
 
-            double circ = Circunferencia(raio);
-            double vol = Volume(raio);
+            Console.WriteLine($"Circunferencia: {Circ1.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture)}");         
 
-            Console.WriteLine($"Circunferencia: {circ.ToString("F2", CultureInfo.InvariantCulture)}");         
-
-            Console.WriteLine($"Volume: {vol.ToString("F2", CultureInfo.InvariantCulture)}");
-            Console.WriteLine($"Pi:{Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Volume: {Circ1.Volume(raio).ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Pi:{Circ1.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
             }
-        static double Circunferencia(double r)
-            {
-            return 2 * Pi * r;
-            }
-        static double Volume(double r)
-            {
-            return 4 * Pi * Math.Pow(r, 3) / 3;
-            }
+        
         }
     }
