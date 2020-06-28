ff639babf5228a3b3a33cfd9bc6a80de5175edb9
Gabriel Braga <araujogabriel2010@gmail.com>
2020-06-27 22:35:20 -0300

    Adicionado o projeto EstaticMembers

---
+++ EstaticMembers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstaticMembers
+    {
+    class Program
+        {
+
+        static double Pi = 3.14;
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Entre com o valor do Raio: ");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circ = Circunferencia(raio);
+
+            Console.WriteLine($"Circunferencia: {circ}");
+
+            double vol = Volume(raio);
+
+            Console.WriteLine($"Volume: {vol.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+        static double Circunferencia(double r)
+            {
+            return 2 * Pi * r;
+            }
+        static double Volume(double r)
+            {
+            return 4 * Pi * Math.Pow(r, 3) / 3;
+            }
+        }
+    }
