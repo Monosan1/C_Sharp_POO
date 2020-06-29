6ca6aebfcf3568690e7a79ef0f4ff7258049dce2
Gabriel Braga <araujogabriel2010@gmail.com>
2020-06-29 09:38:38 -0300

    Membros estáticos criados.

--- EstaticMembers/Program.cs
+++ EstaticMembers/Program.cs
@@ -16,10 +16,10 @@ namespace EstaticMembers
             Console.WriteLine("Entre com o valor do Raio: ");
             double raio = double.Parse(Console.ReadLine());           
 
-            Console.WriteLine($"Circunferencia: {Circ1.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture)}");         
+            Console.WriteLine($"Circunferencia: {Calc.Circunferencia(raio).ToString("F2", CultureInfo.InvariantCulture)}");         
 
-            Console.WriteLine($"Volume: {Circ1.Volume(raio).ToString("F2", CultureInfo.InvariantCulture)}");
-            Console.WriteLine($"Pi:{Circ1.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Volume: {Calc.Volume(raio).ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Pi:{Calc.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
             }
         
         }
